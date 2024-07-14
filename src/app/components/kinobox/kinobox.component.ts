e491c1f1c8be9f42f6f4b4df7ff329da25b389c9
DMALKOV\dmalkov <demoncfh@mail.ru>
2024-07-14 16:54:38 +0400

    правка

--- src/app/components/kinobox/kinobox.component.ts
+++ src/app/components/kinobox/kinobox.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { DOCUMENT } from "@angular/common";
-import { ActivatedRoute } from "@angular/router";
+import {ActivatedRoute, ActivatedRouteSnapshot} from "@angular/router";
 
 @Component({
   selector: 'app-kinobox',
@@ -13,6 +13,7 @@ export class KinoboxComponent implements OnInit {
     @Inject(DOCUMENT) private document: Document,
     private renderer2: Renderer2,
     private route: ActivatedRoute,
+    private route2: ActivatedRouteSnapshot,
   ) { }
 
   ngOnInit(): void {
@@ -22,6 +23,7 @@ export class KinoboxComponent implements OnInit {
     this.renderer2.appendChild(this.document.body, srcScript);
 
     console.log('route', this.route)
+    console.log('route2', this.route2)
     this.route.params.subscribe((params) => {
       console.log('Subdomain', params)
     })
