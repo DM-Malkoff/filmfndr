9ad686875f35505ce4c0afe135f6660e464437a0
DMALKOV\dmalkov <demoncfh@mail.ru>
2024-07-14 15:06:41 +0400

    небольшие правки

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'filmfndr';
-
-  public ngOnInit() {
-    // this.loadScript('https://kinobox.tv/kinobox.min.js').then(() => {
-    //   // Вызов функции kbox после загрузки скрипта
-    //   kbox('.kinobox_player', {search: {kinopoisk: 'ID_ФИЛЬМА'}});
-    // });
-  }
-
-  public loadScript(src: string): Promise<any> {
-    return new Promise((resolve) => {
-      const script = document.createElement('script');
-      script.src = src;
-      script.onload = resolve;
-      document.body.appendChild(script);
-    });
-  }
-
+export class AppComponent {
+  title = 'Онлайн-кинотеатр Filmfndr.ru';
 }
 
