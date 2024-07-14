526d81899422ec2b46128711224e0967f048ad2c
DMALKOV\dmalkov <demoncfh@mail.ru>
2024-07-14 22:57:01 +0400

    небольшая правка

--- environment.ts
+++ environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL: 'https://api.kinopoisk.dev/v1.4/movie/',
+  API_URL: 'https://api.kinopoisk.dev/v1.4/movie',
   API_KEY:  'YW3ZH0Y-H7PM8C4-JB7XFBT-NAY0Q4X',
 }
